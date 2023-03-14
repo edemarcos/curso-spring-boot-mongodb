81b97a3311ae14299ad468b618d61d780c80f1ce
Edemarcos Rossetti <edemarcos.rosetti@gmail.com>
2023-03-14 15:56:40 -0300

    ajustado entidade user para conectar ao mongo

--- src/main/java/com/edecursojava/workshopmongo/domain/User.java
+++ src/main/java/com/edecursojava/workshopmongo/domain/User.java
@@ -6,7 +6,7 @@ import org.springframework.data.mongodb.core.mapping.Document;
 import java.io.Serializable;
 import java.util.Objects;
 
-@Document(collation = "user")
+@Document(collection = "user")
 public class User implements Serializable {
 
     private static final long serialVersionUID = 1L;
--- src/main/java/com/edecursojava/workshopmongo/resource/UserResource.java
+++ src/main/java/com/edecursojava/workshopmongo/resource/UserResource.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
@@ -19,7 +20,7 @@ public class UserResource {
     @Autowired
     private UserService service;
 
-    @GetMapping
+    @RequestMapping(method= RequestMethod.GET)
     public ResponseEntity<List<User>> findAll(){
         List<User> list = service.findAll();
         return ResponseEntity.ok().body(list);
