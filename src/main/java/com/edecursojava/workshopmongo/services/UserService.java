4170b5ed8417055a762ecb76da4147ec8d82faed
edemarcos <edemarcos.rosetti@gmail.com>
2023-03-13 21:55:33 -0300

    Conectando ao mongo com repository e service

---
+++ src/main/java/com/edecursojava/workshopmongo/services/UserService.java
@@ -0,0 +1,18 @@
+package com.edecursojava.workshopmongo.services;
+
+import com.edecursojava.workshopmongo.domain.User;
+import com.edecursojava.workshopmongo.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository repository;
+    public List<User> findAll(){
+        return repository.findAll();
+    }
+}
