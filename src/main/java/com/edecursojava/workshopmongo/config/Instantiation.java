58c4b68afdfefccddb09b33e5d17437ba613443a
Edemarcos Rossetti <edemarcos.rosetti@gmail.com>
2023-03-22 14:59:01 -0300

    projeção dos dados de author com dto

--- src/main/java/com/edecursojava/workshopmongo/config/Instantiation.java
+++ src/main/java/com/edecursojava/workshopmongo/config/Instantiation.java
@@ -2,6 +2,7 @@ package com.edecursojava.workshopmongo.config;
 
 import com.edecursojava.workshopmongo.domain.Post;
 import com.edecursojava.workshopmongo.domain.User;
+import com.edecursojava.workshopmongo.dto.AuthorDTO;
 import com.edecursojava.workshopmongo.repository.PostRepository;
 import com.edecursojava.workshopmongo.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -34,10 +35,12 @@ public class Instantiation implements CommandLineRunner {
         User alex = new User(null, "Alex Green", "alex@gmail.com");
         User bob = new User(null, "Bob Grey", "bob@gmail.com");
 
-        Post post1 = new Post(null, sdf.parse("21/03/2018"), "partiu viagem", "vou viajar ", maria);
-        Post post2 = new Post(null, sdf.parse("21/03/2018"), "partiu viagem", "vou viajar ", maria);
-
         userRepository.saveAll(Arrays.asList(maria, alex, bob));
+
+        Post post1 = new Post(null, sdf.parse("21/03/2018"), "partiu viagem", "vou viajar ", new AuthorDTO(maria) );
+        Post post2 = new Post(null, sdf.parse("21/03/2018"), "partiu viagem", "vou viajar ", new AuthorDTO(maria));
+
+
         postRepository.saveAll(Arrays.asList(post1, post2));
 
     }
--- src/main/java/com/edecursojava/workshopmongo/domain/Post.java
+++ src/main/java/com/edecursojava/workshopmongo/domain/Post.java
@@ -1,5 +1,6 @@
 package com.edecursojava.workshopmongo.domain;
 
+import com.edecursojava.workshopmongo.dto.AuthorDTO;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
@@ -9,19 +10,19 @@ import java.util.Objects;
 
 @Document
 public class Post implements Serializable {
+    private static final long serialVersionUID = 1L;
 
     @Id
-    private static final long serialVersionUID = 1L;
     private String id;
     private Date date;
     private String title;
     private String body;
-    private User author;
+    private AuthorDTO author;
 
     public Post() {
     }
 
-    public Post(String id, Date date, String title, String body, User author) {
+    public Post(String id, Date date, String title, String body, AuthorDTO author) {
         this.id = id;
         this.date = date;
         this.title = title;
@@ -61,11 +62,11 @@ public class Post implements Serializable {
         this.body = body;
     }
 
-    public User getAuthor() {
+    public AuthorDTO getAuthor() {
         return author;
     }
 
-    public void setAuthor(User author) {
+    public void setAuthor(AuthorDTO author) {
         this.author = author;
     }
 
