d44b65467a22a5327e1cf1f8bb93948b06948c0d
Edemarcos Rossetti <edemarcos.rosetti@gmail.com>
2023-03-23 11:17:35 -0300

    acrescentando comentarios aos posts

--- src/main/java/com/edecursojava/workshopmongo/config/Instantiation.java
+++ src/main/java/com/edecursojava/workshopmongo/config/Instantiation.java
@@ -3,6 +3,7 @@ package com.edecursojava.workshopmongo.config;
 import com.edecursojava.workshopmongo.domain.Post;
 import com.edecursojava.workshopmongo.domain.User;
 import com.edecursojava.workshopmongo.dto.AuthorDTO;
+import com.edecursojava.workshopmongo.dto.CommentDTO;
 import com.edecursojava.workshopmongo.repository.PostRepository;
 import com.edecursojava.workshopmongo.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -40,6 +41,12 @@ public class Instantiation implements CommandLineRunner {
         Post post1 = new Post(null, sdf.parse("21/03/2018"), "partiu viagem", "vou viajar ", new AuthorDTO(maria) );
         Post post2 = new Post(null, sdf.parse("21/03/2018"), "partiu viagem", "vou viajar ", new AuthorDTO(maria));
 
+        CommentDTO c1 = new CommentDTO("Boa viagem mano", sdf.parse("21/03/2018"), new AuthorDTO(alex));
+        CommentDTO c2 = new CommentDTO("Aproveite", sdf.parse("22/03/2018"), new AuthorDTO(bob));
+        CommentDTO c3 = new CommentDTO("tenha um otimo dia", sdf.parse("23/03/2018"), new AuthorDTO(maria));
+
+        post1.getComments().addAll(Arrays.asList(c1,c2));
+        post2.getComments().add(c3);
 
         postRepository.saveAll(Arrays.asList(post1, post2));
 
