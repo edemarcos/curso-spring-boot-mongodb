5516a7e2678991a4083a0773c36c267aea47980a
edemarcos <edemarcos.rosetti@gmail.com>
2023-03-23 21:04:34 -0300

    consulta simples com query methods

--- src/main/java/com/edecursojava/workshopmongo/resource/PostResource.java
+++ src/main/java/com/edecursojava/workshopmongo/resource/PostResource.java
@@ -1,11 +1,13 @@
 package com.edecursojava.workshopmongo.resource;
 
 import com.edecursojava.workshopmongo.domain.Post;
+import com.edecursojava.workshopmongo.resource.util.URL;
 import com.edecursojava.workshopmongo.services.PostService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.Optional;
 
 @RestController
@@ -19,6 +21,13 @@ public class PostResource {
     public ResponseEntity<Optional<Post>> findById(@PathVariable String id){
         Optional<Post> obj = service.findById(id);
         return ResponseEntity.ok().body(obj);
+    }
+
+    @RequestMapping(value = "/titlesearch", method= RequestMethod.GET)
+    public ResponseEntity<List<Post>> findByTitle(@RequestParam(value = "text", defaultValue = "") String text){
+        text = URL.decodeParam(text);
+        List<Post> list = service.findByTitle(text);
+        return ResponseEntity.ok().body(list);
     }
 
 
--- src/main/java/com/edecursojava/workshopmongo/services/PostService.java
+++ src/main/java/com/edecursojava/workshopmongo/services/PostService.java
@@ -6,6 +6,7 @@ import com.edecursojava.workshopmongo.services.exception.ObjectNotFoundException
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -20,6 +21,10 @@ public class PostService {
             throw new ObjectNotFoundException("objeto não encontrado");
         }
         return user;
+    }
+
+    public List<Post> findByTitle(String text){
+        return repository.findByTitleContainingIgnoreCase(text);
     }
 
 
